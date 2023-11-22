e9fb9af8d7ed65456e7875306bdfc9d96f26e831
Lukas Prochazka <pro41216@spseol.cz>
2023-11-22 14:10:38 +0100

    idk

--- start.py
+++ start.py
@@ -21,11 +21,13 @@ window = pyglet.window.Window(width=800, height=600)
 batch = pyglet.graphics.Batch()  # pro optimalizované vyreslování objektů
 
 
-class Space(object):
+class Space(pyglet.sprite.Sprite):
     speed_x = 0
     speed_y = 0
 
-    def __init__(self, img_path: str, speed_x = 0, speed_y = 0):
+    def __init__(self, img_path: str, speed_x = 0, speed_y = 0, position_x = 0, position_y = 0, batch=batch):
+        self.img = pyglet.image.load(img_path)
+        super().__init__(img=self.img, batch=batch)
         self.speed_x = speed_x
         self.speed_y = speed_y
 
@@ -34,9 +36,19 @@ class Space(object):
         self.img.anchor_y = self.img.height // 2
         self.sprite = pyglet.sprite.Sprite(self.img, batch=batch)
 
+        self.sprite.x = position_x
+        self.sprite.y = position_y
+
     def move(self, dt: float):
         self.sprite.x += self.speed_x * dt * FPS/2
         self.sprite.y += self.speed_y * dt * FPS/2
+
+class Meteor(Space):
+    speed_x = 500
+    speed_y = 500
+
+    def __init__(self, position_x, position_y, batch):
+        super().__init__(img_path="img/meteorBrown_big1.png", position_x = position_x, position_y = position_y, batch=batch)
 
 @window.event
 def on_draw():
@@ -50,29 +62,38 @@ def on_key_press(sym, mod):
 @window.event
 def on_key_release(sym, mod):
     print(sym, mod)
-
+"""
 @window.event
 def on_mouse_press(x, y, button, mod):
     print(x, y, button)
     ship.x = x
     ship.y = y
-
+"""
 def tick(dt):
+    """
     suter.move(dt)
     bubble.move(dt)
     bubble2.move(dt)
+    
     for o in objekty:
         o.move(dt)
+    """
 
+meteor1 = Meteor(position_x = 50, position_y = 50, batch=batch)
+meteor2 = Meteor(position_x = 150, position_y = 50, batch=batch)
+meteor3 = Meteor(position_x = 250, position_y = 50, batch=batch)
 
+"""
 suter = Space("img/meteorBrown_big1.png", 10, 4)
 bubble = Space("img/bubble.png", 4, 8)
 bubble2 = Space("img/bubble.png", 4*2, 8*2)
+
+
 objekty = []
 for _ in range(22):
     o = Space(random.choice(glob.glob("img/*.png")), random.randint(0,20), random.randint(0,20))
     objekty.append(o)
-
+"""
 # funkce tick se spustí 30x za sekundu
 pyglet.clock.schedule_interval(tick, 1 / FPS)
 
