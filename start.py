75008fd0d8746acf528d79d9df0a4339d7f97cc6
Lukas Prochazka <pro41216@spseol.cz>
2023-12-02 20:45:19 +0100

    Grey*2speed+window_bounce

--- start.py
+++ start.py
@@ -24,6 +24,7 @@ batch = pyglet.graphics.Batch()  # pro optimalizované vyreslování objektů
 class Space(pyglet.sprite.Sprite):
     speed_x = 0
     speed_y = 0
+    speed_modifier = 1
 
     def __init__(self, img_path: str, speed_x = 0, speed_y = 0, position_x = 0, position_y = 0, batch=batch):
         self.img = pyglet.image.load(img_path)
@@ -37,8 +38,25 @@ class Space(pyglet.sprite.Sprite):
         self.img.anchor_y = self.img.height // 2
 
     def move(self, dt: float):
-        self.x += self.speed_x * dt * FPS/2
-        self.y += self.speed_y * dt * FPS/2
+        self.x += self.speed_x * dt * FPS/2 * self.speed_modifier
+        self.y += self.speed_y * dt * FPS/2 * self.speed_modifier
+        print(self.x)
+
+        if self.x > (window.width - self.img.width):
+            self.x = window.width - self.img.width
+            self.speed_x = -self.speed_x
+
+        elif self.y > (window.height - self.img.height):
+            self.y = window.height - self.img.height
+            self.speed_y = -self.speed_y
+        
+        elif self.x < 0:
+            self.x = 0
+            self.speed_x = -self.speed_x
+
+        elif self.y < 0:
+            self.y = 0
+            self.speed_y = -self.speed_y
 
 class Meteor(Space):
     img_path = None
@@ -48,14 +66,15 @@ class Meteor(Space):
         self.x = position_x
         self.y = position_y
 
-        self.speed_x = random.randint(0,10)
-        self.speed_y = random.randint(0,10)
+        self.speed_x = random.randint(1,10)
+        self.speed_y = random.randint(1,10)
 
 class MeteorBrown(Meteor):
     img_path = "img/meteorBrown_big1.png"
 
 class MeteorGray(Meteor):
     img_path="img/meteorGrey_big1.png"
+    speed_modifier = 2
 
 @window.event
 def on_draw():
@@ -78,36 +97,16 @@ def on_mouse_press(x, y, button, mod):
     ship.y = y
 """
 def tick(dt):
-    meteor1.move(dt)
-    meteor2.move(dt)
-    meteor3.move(dt)
-
-
-    """
-    suter.move(dt)
-    bubble.move(dt)
-    bubble2.move(dt)
+    for Object in Objects:
+        Object.move(dt)
     
-    for o in objekty:
-        o.move(dt)
-    """
+Objects = []
+Meteors = [MeteorBrown, MeteorGray]
+for _ in range(1):
+    Type = random.choice(Meteors)
+    Object = Type(position_x = random.randint(0,600), position_y = random.randint(0,800), batch=batch)
+    Objects.append(Object)
 
-meteor1 = MeteorBrown(position_x = 50, position_y = 50, batch=batch)
-meteor2 = MeteorBrown(position_x = 150, position_y = 50, batch=batch)
-meteor3 = MeteorBrown(position_x = 250, position_y = 50, batch=batch)
-meteor4 = MeteorGray(position_x = 450, position_y = 50, batch=batch)
-
-"""
-suter = Space("img/meteorBrown_big1.png", 10, 4)
-bubble = Space("img/bubble.png", 4, 8)
-bubble2 = Space("img/bubble.png", 4*2, 8*2)
-
-
-objekty = []
-for _ in range(22):
-    o = Space(random.choice(glob.glob("img/*.png")), random.randint(0,20), random.randint(0,20))
-    objekty.append(o)
-"""
 # funkce tick se spustí 30x za sekundu
 pyglet.clock.schedule_interval(tick, 1 / FPS)
 
